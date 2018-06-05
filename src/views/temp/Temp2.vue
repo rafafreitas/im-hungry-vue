3152a70699af5e72a9313d6520f6530e221d0e0e
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-06-05 12:23:50 -0300

    Pages | Implementado páginas para realizar a codificação.

---
+++ src/views/temp/Temp2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <div class="animated fadeIn">
+      <b-row>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Temp2',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
